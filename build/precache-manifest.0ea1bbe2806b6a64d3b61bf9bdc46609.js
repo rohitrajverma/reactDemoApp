c1b3357b742512d3ab5d6e897190363a7878e325
rohitrajverma <rohitrajverma1611@gmail.com>
2020-09-13 15:23:22 +0530

    new build

--- build/precache-manifest.0ea1bbe2806b6a64d3b61bf9bdc46609.js
+++ build/precache-manifest.0ea1bbe2806b6a64d3b61bf9bdc46609.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f62c5c18d7b2a10219cd16ecbac51d17",
+    "revision": "0807e210594001b7ecf5933d909866f4",
     "url": "/reactDemoApp/index.html"
   },
   {
-    "revision": "95925dbdcf4b5c887fd0",
-    "url": "/reactDemoApp/static/css/main.7ad9603b.chunk.css"
+    "revision": "1173c813e01b540d4bd2",
+    "url": "/reactDemoApp/static/css/main.05d1216e.chunk.css"
   },
   {
-    "revision": "8d7577ce38365424a8c8",
-    "url": "/reactDemoApp/static/js/2.513ea8fa.chunk.js"
+    "revision": "96ea1f2accd663fd6fbf",
+    "url": "/reactDemoApp/static/js/2.888e3529.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/reactDemoApp/static/js/2.513ea8fa.chunk.js.LICENSE.txt"
+    "url": "/reactDemoApp/static/js/2.888e3529.chunk.js.LICENSE.txt"
   },
   {
     "revision": "e14b6557b9ee91c170ec",
     "url": "/reactDemoApp/static/js/3.194cf6ed.chunk.js"
   },
   {
-    "revision": "95925dbdcf4b5c887fd0",
-    "url": "/reactDemoApp/static/js/main.d4f67dc7.chunk.js"
+    "revision": "1173c813e01b540d4bd2",
+    "url": "/reactDemoApp/static/js/main.4b224f41.chunk.js"
   },
   {
     "revision": "591ce23cc65c0ce83383",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactDemoApp/precache-manifest.2f28998d1baeeae18a4aabfee3211f07.js"
+  "/reactDemoApp/precache-manifest.0ea1bbe2806b6a64d3b61bf9bdc46609.js"
 );
 
 self.addEventListener('message', (event) => {
